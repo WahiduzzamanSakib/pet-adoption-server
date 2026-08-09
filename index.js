8d353979700984c8020eedf5176d35265fade274
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-08-09 11:18:47 +0600

    Update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 });
 
 const JWKS = createRemoteJWKSet(
-    new URL("http://localhost:3000/api/auth/jwks")
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
 )
 
 const verifyToken = async (req, res, next) => {
@@ -43,7 +43,6 @@ const verifyToken = async (req, res, next) => {
 
     try {
         const { payload } = await jwtVerify(token, JWKS);
-        console.log(payload);
         next();
     } catch (error) {
         return res.status(403).json({ message: "Forbidden" });
@@ -53,7 +52,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
         const db = client.db("pet-adoption")
         const petCollection = db.collection("all-pets")
@@ -225,7 +224,7 @@ async function run() {
         })
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
